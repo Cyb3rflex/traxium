8b8bf6f47459128a0e584769d100d7cea8c5a9b9
Eeinayoann <okeyodeann@gmail.com>
2024-10-09 15:14:39 +0100

    made new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import logo from './logo.svg';
+// import './App.css';
+// import MyComponent from './image'
+import { BrowserRouter,Route,Routes} from 'react-router-dom';
+import SignUp from '../src/component/signup';
+import Signin from './component/signin';
 
 function App() {
-  return (
-    <div>App</div>
-  )
+return (
+  <BrowserRouter>
+  <div>
+    <Routes>
+      {/* hello */}
+    {/* <MyComponent/> */}
+    <Route path='/' element={<SignUp/>}/>
+    <Route path='/signup' element={<SignUp/>}/>
+    <Route path='/signin' element={<Signin/>}/>
+    
+    </Routes>
+    {/* <SignUp/> */}
+    {/* <Signin/> */}
+  </div>
+  </BrowserRouter>
+)
 }
 
 export default App;
