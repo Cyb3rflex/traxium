e60edac676a9c02c15f348a08f9abf6092032222
Eeinayoann <okeyodeann@gmail.com>
2024-10-09 17:00:11 +0100

    undated

--- src/Components/signup.jsx
+++ src/Components/signup.jsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react' 
 import '../component/signup.css'
 import image from '../Images/image.jpg'
-
 import { Link } from "react-router-dom";
 import SignIn from './Signin';
 
