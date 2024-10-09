d9835cd41c9a0eaa866bffaeb91b8e2dc08bdd40
Eeinayoann <okeyodeann@gmail.com>
2024-10-09 13:29:55 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,8 @@
-import { useState } from "react";
-import './App.css';
+import React from 'react'
 
 function App() {
-  const [age, setAge] = useState(0);
-
-  function btnClicked() {
-    setAge(age+1);
-  }
-  // function btnClickedMinus() {
-  //   setAge(age-1);
-  // }
-  function btnClickedMinus() {
-    setAge((age) => (age > 0 ? age - 1 : 0));
-  }
-  
-  const btnReset = () => {
-    setAge(0)
-  }
-
   return (
-    <div>
-      <p >{age}</p>
-      <button onClick={btnClicked} className>Addition</button>
-      <button onClick={btnReset}>Reset</button>
-      <button onClick={btnClickedMinus}>Subtraction</button>
-    </div>
+    <div>App</div>
   )
 }
 
---
+++ src/Components/Signin.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import image from '../image.jpg'
+import './signin.css'
+import photo from '../photo.jpg'
+import SignUp from "./signup";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+
+const SignIn = () => {
+   const[email, setEmail] = useState('');
+   const[password, setPassword] = useState('');
+   const[message, setMessage] = useState('');
+
+   const handleSignIn = (e) =>{
+    e.preventDefault();
+    if (email === 'user@example.com' && password === 'password123') {
+        setMessage('sign in successful!')
+    } else{
+        setMessage('Invalid email or password')
+    }
+   }
+    return(
+        <div className="option">
+            <div id="header">
+                <h1 id="head">Sign in to KWARA RIDE</h1>
+                <img className="image" src={image} alt="" />
+            </div>
+            <button id="but">
+                <img src={photo} alt="" />
+            sign in with Google
+            </button> <br/>
+            
+            <form onSubmit={handleSignIn}>
+            <div>
+             <label> Username or Email</label> 
+             <br />
+             <input
+             id="value" 
+             type="email"
+             value={email} 
+             onChange ={(e) => setEmail(e.target.value)}
+             required
+             />
+            </div>
+            <br />
+            <div>
+                <label>Password</label> <br />
+                <input 
+                id="word"
+                type="password" 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                />
+            </div>
+            <br />
+          
+            </form>
+           
+
+          {message && <p>{message}</p>}
+
+          <button id="type" type="submit">sign in</button>
+            <p id="have">Don't have an account? <a href="./signup"></a>
+            <Link to='/signup' rel="">sign up</Link>
+            </p>
+        </div>
+    )
+}
+ 
+export default SignIn;
