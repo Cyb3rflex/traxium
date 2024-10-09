d7349365ced23d24b9419b7b7f2d0986ec697e35
Abdulmuheez <abdulmuheezq@gmail.com>
2024-10-09 13:58:06 +0100

    update signup page and remove unused components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /* @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3498db",
+        secondary: "#f1c40f",
+      },
+    },
   },
   plugins: [],
 };
