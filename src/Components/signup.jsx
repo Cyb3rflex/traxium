b838a4634b0a81943b541eb4eb917ee8f454c1a6
Abdulmuheez <abdulmuheezq@gmail.com>
2024-10-14 14:14:20 +0100

    another one

--- src/Components/signup.jsx
+++ src/Components/signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "../component/signup.css";
 import image from "../Images/image.jpg";
 import { useNavigate } from "react-router-dom";
